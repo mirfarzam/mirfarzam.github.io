9724d787f1a51ead7f54542a4788981fcbe7a76b
mirfarzam <farzam.mirmoeini@gmail.com>
2020-02-29 18:36:37 +0100

    set name

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,43 @@
-function readJSON(path) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', path, true);
-    xhr.responseType = 'blob';
-    xhr.onload = function(e) {
-      if (this.status == 200) {
-          var file = new File([this.response], 'temp');
-          var fileReader = new FileReader();
-          fileReader.addEventListener('load', function(){
-               //do stuff with fileReader.result
-          });
-          fileReader.readAsText(file);
-      }
-    }
-    xhr.send();
+
+function getLanguage(codes, code) {
+  if(code == null) code = geoplugin_countryCode()
+  if(codes.indexOf(code) > -1) return code;
+  else return "EN";
 }
 
-function feedSite() {
-  var langKey = geoplugin_countryCode()
-  var language = readJSON("language.json")
-  console.log(language)
+function initLanguage() {
+  fetch('https://mirfarzam.ninja/scripts/language.json')
+    .then(res => res.json())
+    .then((out) => {
+        window.language = out;
+        var code = getLanguage(Object.keys(window.language), null);
+        var contentLang = window.language[code];
+        $('select option[value="'+code+'"]').attr("selected",true);
+        feedSite(code, contentLang);
+      }).catch(err => console.error(err));
 }
 
-feedSite()
+function feedSite(code, contentLang){
+  var content = $("#content")
+  // Check for Farsi
+  if(code=="IR") {
+    content.css("font-family", "shabnam")
+    content.css("direction", "rtl")
+  } else {
+    content.css("font-family", "Source Sans Pro")
+    content.css("direction", "ltr")
+  }
+  // Filling the placeholders
+  content.find("#name").html(contentLang["name"])
+  content.find("#nickname").html(contentLang["nickname"])
+  content.find("#about").html(contentLang["about"])
+}
+
+function changeLanguage(){
+  var code = getLanguage(Object.keys(window.language), $("#languageSelector").val());
+  var contentLang = window.language[code];
+  feedSite(code, contentLang);
+}
+
+
+initLanguage();

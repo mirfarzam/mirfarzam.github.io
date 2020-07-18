deb2b9b0935de18c66857fe975994023c72e526a
Seyed Farzam Mirmoeini <farzam.mirmoeini@gmail.com>
2020-07-18 15:01:07 +0430

    Updates

---
+++ precache-manifest.1431bbe562dc376e636233667732e848.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39169dc7a3c0058388c10f87dda6139a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb78147ae29ba251460b",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "5e50737fa41f01b2866f",
+    "url": "/static/css/main.1eea0ca0.chunk.css"
+  },
+  {
+    "revision": "eb78147ae29ba251460b",
+    "url": "/static/js/2.5d48beff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5d48beff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e50737fa41f01b2866f",
+    "url": "/static/js/main.43b291c8.chunk.js"
+  },
+  {
+    "revision": "cb6bc207c492fcd27aac",
+    "url": "/static/js/runtime-main.4260dab8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
